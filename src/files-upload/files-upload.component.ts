a2546c5ecc2243eb14ce36bfa495aaebd8799ed4
mvdrax <127264906+mvdrax@users.noreply.github.com>
2025-05-05 17:50:37 +0200

    Add files via upload

--- src/files-upload/files-upload.component.ts
+++ src/files-upload/files-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ChangeDetectorRef  } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { FileService } from '../services/files-service/files.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -8,18 +8,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { UploadFileUserComponent } from '../app/upload-file-user/upload-file-user.component';
 
-
-
 @Component({
   selector: 'app-files-upload',
-  imports: [ 
- CommonModule,
- FormsModule, ReactiveFormsModule,
- MatDatepickerModule,
- MatNativeDateModule,
-     MatFormFieldModule,
-     MatInputModule,
-     UploadFileUserComponent
+  imports: [
+    CommonModule,
+    FormsModule, ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    UploadFileUserComponent
   ],
   standalone: true,
   templateUrl: './files-upload.component.html',
@@ -29,92 +27,69 @@ import { UploadFileUserComponent } from '../app/upload-file-user/upload-file-use
 export class FilesUploadComponent {
 
   isFormOpen = false;
-
-  tab: number =1;
+  tab: number = 1;
 
   filesListByAgencyFiche: any[] = [];
   filesListByAgencyFact: any[] = [];
-
   newFilteredList: any[] = [];
 
-  uploadedFiles: any[] = []; 
-
-  get mergedList(): any[] {
-    return [...this.uploadedFiles, ...this.newFilteredList];
-  }
-
-  searchQueryFactures: string = ''; 
+  searchQueryFactures: string = '';
   searchQueryFiches: string = '';
   invoices: any[] = [];
   paySlips: any[] = [];
-  startDateFactures: Date | null = null; 
-  endDateFactures: Date | null = null;   
+
+  startDateFactures: Date | null = null;
+  endDateFactures: Date | null = null;
+
+  startDateFiches: Date | null = null;
+  endDateFiches: Date | null = null;
+
+  listAgences: any[] = [];
+  showAgences = false;
+
+  filesListByAgency: any[] = [];
+  selectedAgence: string = '';
+  agence: string = '';
+
+  blobUrls: { [key: string]: string } = {};
+
+
+
   
-  startDateFiches: Date | null = null;   
-  endDateFiches: Date | null = null; 
 
- pdfFiles: any[] = [];
-
- listAgences: any[] = [];
- showAgences = false;
-
- filesListByAgency:  any[] = [];
- selectedAgence: string = ''
- agence: string ='';
-
- blobUrls: { [key: string]: string } = {}; //dernier ajout 
 
   constructor(private pdfFilesService: FileService, private cdr: ChangeDetectorRef) {}
-    
-  
+
   ngOnInit(): void {
-
-    
-
-
-
     this.pdfFilesService.getAgencies().subscribe(data => {
-      console.log('Réponse brute de l API:', data);
-      this.listAgences = data.sort((a,b) => a.localeCompare(b));
-
+      this.listAgences = data.sort((a, b) => a.localeCompare(b));
       this.selectedAgence = this.listAgences[0];
-
       this.loadingFilesByAgency(this.selectedAgence);
-       console.log(this.selectedAgence)
-
-
-
     });
-
-
-    this.mergedList.forEach(item => {  //dernier ajout 
-
-      if (item.docFile) {
-        this.blobUrls[item.docFile] = this.getBlobUrl(item.docFile);
-      }
-    });
-  
-
   }
-
 
   agenceChanging(agence: string) {
-    console.log('fct agencechanging appelée')
-    this.loadingFilesByAgency(agence)
+    this.loadingFilesByAgency(agence);
   }
 
+  loadingFilesByAgency(agence: string) {
+    this.pdfFilesService.getPdfFilesByAgency(agence).subscribe(data => {
+      this.filesListByAgency = data;
+      this.filterFilesByTab();
 
- loadingFilesByAgency(agence: string) {
-  console.log('fct loading appelée')
-  this.pdfFilesService.getPdfFilesByAgency(agence).subscribe(data => {
-    this.filesListByAgency = data;
-    console.log(data)
-  })
-
- }
+      this.generateMonthYearOptions();
 
 
-   
+      // Met à jour les blobUrls
+      this.blobUrls = {};
+      this.filesListByAgency.forEach(item => {
+        if (item.docFile) {
+          this.blobUrls[item.docFile] = this.getBlobUrl(item.docFile);
+        }
+      });
+    });
+  }
+
   openAddForm() {
     this.isFormOpen = true;
   }
@@ -122,240 +97,185 @@ export class FilesUploadComponent {
   closeAddForm() {
     this.isFormOpen = false;
   }
-  
 
-
-  onFileUploaded(newFile: any) {
-    console.log("Nouveau fichier  :", newFile);
-    this.uploadedFiles.push(newFile); 
-    this. filterFilesByTab();
-  }
-
-
-  
-
-
-
-
-
-  //gérer l'alternance des ongletet les données affichées
-  
   onTabChange(tab: number) {
     this.tab = tab;
-    this.filterFilesByTab(); 
+    this.filterFilesByTab();
+
+    this.generateMonthYearOptions();
+
+    this.selectedMonthYear = '';
   }
-  
+
   filterFilesByTab() {
-
-  
     if (this.tab === 1) {
-      this.filesListByAgencyFact = this.filesListByAgency.filter(item => item.fileType === 'Fact');
+      this.newFilteredList = this.filesListByAgency.filter(item => item.fileType === 'Fact');
     } else if (this.tab === 2) {
-      this.filesListByAgencyFiche = this.filesListByAgency.filter(item => item.fileType === 'Fiche');
+      this.newFilteredList = this.filesListByAgency.filter(item => item.fileType === 'Paie');
     }
-    console.log('Fichiers après filtrage:', this.filesListByAgency);
-  
-    }
-  
-  
+  }
 
-
-
-  // filtrer les pdf selon leur type (facture ou fiche de paie)
-  
   getBlobUrl(docFile: string): string {
-  const b64chain = docFile; 
-  console.log('chaine vld');
-const newb4chain = b64chain.replace(/['"]/g, '');
-  if (!newb4chain) {
-    throw new Error('La chaîne Base64 est invalide.');
-    console.error('chaine invalide');
-  }
-
-  try {
-    console.log('Début du décodage...');
-    const db64chain = atob(newb4chain); // Tentative de décodage Base64
-    console.log('Chaîne décodée avec succès');
-
-  const byteArrays = new Uint8Array(db64chain.length); //création tableau d'octets
-  for (let i = 0; i <  db64chain.length; i++) {
-    byteArrays[i] = db64chain.charCodeAt(i); //charCodeAt(i) renvoie le code de caractère Unicode (un nombre entier entre 0 et 65535) du caractère situé à l'index i de la chaîne b64chain
-  }
-  console.log('Tableau d\'octets:', byteArrays);
-
-
-  
-  
-
-
-  const blob = new Blob([byteArrays], { type: 'application/pdf'}); //création d'une url blob
-  console.log('Blob créé:', blob);
-  const url = URL.createObjectURL(blob);
-
-console.log('URL générée:', url);
-
-
-
-return url;
-
-  } catch (error) {
-    console.error('Erreur lors du décodage Base64:', error);
-    throw new Error('Erreur de décodage Base64');
-  }
-
-  }
-  
-  
-  
-  
-  //filtrer les résultats par rapport à la recherche textuelle
-  
-  filteredSearch(fileType: string) {
-  
-  
-    const filteredListByType = this.pdfFiles.filter( item => item.fileType === fileType);
-  
-  if (fileType === 'Fact') {
-    this.newFilteredList  = !this.searchQueryFactures ? filteredListByType : filteredListByType.filter
-    (item => item.tiersName.toLowerCase().includes(this.searchQueryFactures.toLowerCase())
-  );
-  } else if (fileType === 'Fiche') {
-    this.newFilteredList = !this.searchQueryFiches ? filteredListByType : filteredListByType.filter
-    (item => item.tiersName.toLowerCase().includes(this.searchQueryFiches.toLowerCase())
-  );
-  }
-  }
-  
-  
-
-
-
-// fusion des listes filtrées et des valeurs juste ajoutées
-
-
-
-  
-  
-  applyDateFilter(fileType: string) {
-  
-    if (fileType === 'Fact') {
-    if (this.startDateFactures && this.endDateFactures) {
-      if (this.startDateFactures > this.endDateFactures) {
-        alert ("Dates non conformes");
-        return;
-      }
-  
-      this.newFilteredList = this.pdfFiles.filter(item => {
-        const itemDate = new Date(item.dateFile);
-        return  (
-          item.fileType === 'Fact' &&
-          itemDate >= this.startDateFactures! && 
-          itemDate <= this.endDateFactures!
-        );
-      });
-    
-    } else {
-      alert("Champ vide");
+    const b64chain = docFile.replace(/['"]/g, '');
+    if (!b64chain) {
+      throw new Error('La chaîne Base64 est invalide.');
     }
+
+    try {
+      const db64chain = atob(b64chain);
+      const byteArrays = new Uint8Array(db64chain.length);
+      for (let i = 0; i < db64chain.length; i++) {
+        byteArrays[i] = db64chain.charCodeAt(i);
+      }
+
+      const blob = new Blob([byteArrays], { type: 'application/pdf' });
+      return URL.createObjectURL(blob);
+    } catch (error) {
+      console.error('Erreur lors du décodage Base64:', error);
+      throw new Error('Erreur de décodage Base64');
+    }
+  }
+
+
+ /* openPdf(docFile: string, filename: string): void {
+    const urlBlob = this.getBlobUrl(docFile);
+    const newWin = window.open(urlBlob, '_blank');
+    newWin.document.title = filename;
+    newWin.document.body.innerHTML = `
+      <iframe width="100%" height="100%" src="${urlBlob}" frameborder="0"></iframe>
+    `;
+  }*/
+
+  filteredSearch(fileType: string) {
+
+    const filteredListByType = this.filesListByAgency.filter(item => item.fileType === fileType);
+    console.log('recherche', filteredListByType)
+
+    if (fileType === 'Fact') {
+      this.newFilteredList = !this.searchQueryFactures
+        ? filteredListByType
+        : filteredListByType.filter(item =>
+            item.tiersName.toLowerCase().includes(this.searchQueryFactures.toLowerCase())
+          );
+
+          console.log('results sear', this.newFilteredList)
+    } else if (fileType === 'Paie') {
+      this.newFilteredList = !this.searchQueryFiches
+        ? filteredListByType
+        : filteredListByType.filter(item =>
+            item.tiersName.toLowerCase().includes(this.searchQueryFiches.toLowerCase())
+          );
+    }
+
+    this.cdr.detectChanges();
+  }
+
+  applyDateFilter(fileType: string) {
+    if (fileType === 'Fact') {
+      if (this.startDateFactures && this.endDateFactures) {
+        if (this.startDateFactures > this.endDateFactures) {
+          alert("Dates non conformes");
+          return;
+        }
+
+        this.newFilteredList = this.filesListByAgency.filter(item => {
+          const itemDate = new Date(item.dateFile);
+          return (
+            item.fileType === 'Fact' &&
+            itemDate >= this.startDateFactures! &&
+            itemDate <= this.endDateFactures!
+          );
+        });
+      } else {
+        alert("Champ vide");
+      }
     } else if (fileType === 'Fiche') {
       if (this.startDateFiches && this.endDateFiches) {
         if (this.startDateFiches > this.endDateFiches) {
-          alert ("Dates non conformes");
-        return;
+          alert("Dates non conformes");
+          return;
         }
-        this.newFilteredList = this.pdfFiles.filter(item => {
+
+        this.newFilteredList = this.filesListByAgency.filter(item => {
           const itemDate = new Date(item.dateFile);
           return (
-            item.fileType === 'Fiche' && 
+            item.fileType === 'Fiche' &&
             itemDate >= this.startDateFiches! &&
-            itemDate <= this.endDateFiches! 
+            itemDate <= this.endDateFiches!
           );
         });
       } else {
         alert('Veuillez sélectionner les deux dates pour les fiches de paie !');
       }
     }
-  
   }
 
-
   resetDatesFact() {
-   
     this.startDateFactures = null;
     this.endDateFactures = null;
-
-    this.filterFilesByTab(); 
-
+    this.filterFilesByTab();
   }
 
   resetDatesFiches() {
-   
     this.startDateFiches = null;
     this.endDateFiches = null;
-
-    this.filterFilesByTab(); 
-
+    this.filterFilesByTab();
   }
 
+  toggleAgences() {
+    this.showAgences = !this.showAgences;
 
-
-
-
-// Déclencher menu déroulant pour choisir une agence
-
-
-toggleAgences() {
-
- 
-  
-this.showAgences = !this.showAgences
-
-if (this.showAgences && this.listAgences.length === 0 ) {
-
-  this.pdfFilesService.getAgencies().subscribe(data => {
-    console.log('Réponse  AGENCES brute de l API:', data);
-    this.listAgences = data;
-
-});
-
-/*
-  const optionDft = document.createElement("option");
-  optionDft.text = "-- Sélectionnez une agence --";
-  optionDft.disabled = true;
-  optionDft.selected = true;
-  select?.appendChild(optionDft);
-
-
-
-  
-  this.pdfFilesService.getAgencies().subscribe(data => {
-    console.log('Réponse  AGENCES brute de l API:', data);
-    this.listAgences = data;
-    data.forEach(agence => {
-      const option = document.createElement('option');
-      option.value = agence;
-      option.text = agence;
-      select?.appendChild(option);
-      
-    }); 
-  
-  });   */ 
-   
+    if (this.showAgences && this.listAgences.length === 0) {
+      this.pdfFilesService.getAgencies().subscribe(data => {
+        this.listAgences = data;
+      });
+    }
   }
+// Pour la sélection du mois/année
 
 
+selectedMonthYear: string = '';
+monthYearOptions: string[] = [];
 
+generateMonthYearOptions(): void {
+ // const factures = this.filesListByAgency.filter(item => item.fileType === 'Fact');
+
+  const uniqueMonthYears = new Set<string>();
+  this.newFilteredList.forEach(fact => {
+    const date = new Date(fact.dateFile);
+    const monthYear = `${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
+    uniqueMonthYears.add(monthYear);
+  });
+
+  this.monthYearOptions = Array.from(uniqueMonthYears).sort((a, b) => {
+    const [monthA, yearA] = a.split('/').map(Number);
+    const [monthB, yearB] = b.split('/').map(Number);
+    return yearB - yearA || monthB - monthA;
+  });
 }
 
+onMonthYearChange(): void {
+  if (!this.selectedMonthYear) {
+    this.filterFilesByTab(); 
+    return;
+  }
 
+  const [selectedMonth, selectedYear] = this.selectedMonthYear.split('/').map(Number);
 
+  const currentTabFileType = this.tab === 1 ? 'Fact' : 'Paie';
 
+  this.newFilteredList = this.filesListByAgency.filter(item => {
+    const itemDate = new Date(item.dateFile);
+    return (
+      item.fileType === currentTabFileType  &&
+      itemDate.getMonth() + 1 === selectedMonth &&
+      itemDate.getFullYear() === selectedYear
 
+        
 
-
-
-
-
+    );
+  });
+}
 
 
 
